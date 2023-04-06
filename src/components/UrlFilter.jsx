f51f16051393c269ebfb4637ffbf9cc878be9952
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2023-04-06 15:18:41 +0530

    feat:update filter

--- src/components/UrlFilter.jsx
+++ src/components/UrlFilter.jsx
@@ -1,25 +1,25 @@
 import {
+  Box,
   Checkbox,
   CheckboxGroup,
   Flex,
+  FormLabel,
   IconButton,
   Input,
   Menu,
   MenuButton,
   MenuDivider,
   MenuGroup,
-  MenuItem,
   MenuList,
   Radio,
   RadioGroup,
   Stack,
-  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import { FaFilter } from "react-icons/fa";
 const UrlFilter = () => {
   return (
-    <Menu closeOnSelect={false}>
+    <Menu closeOnSelect={false} placement="bottom-end">
       <MenuButton
         as={IconButton}
         aria-label="Options"
@@ -27,9 +27,10 @@ const UrlFilter = () => {
         variant="solid"
         colorScheme="green"
       />
-      <MenuList>
-          {/* FULL PAGE SCREENSHOT*/}
-        <MenuGroup title="Screenshot size" type="checkbox">
+      <MenuList height={"70vh"} overflowY={"scroll"} p='10px'>
+        {/* FULL PAGE SCREENSHOT*/}
+        <MenuGroup title="Screenshot 
+        Size" type="checkbox">
           <CheckboxGroup colorScheme="green">
             <Stack spacing={"5"} p="10px">
               <Checkbox>Capture full page</Checkbox>
@@ -37,17 +38,19 @@ const UrlFilter = () => {
           </CheckboxGroup>
 
           {/* SCREENSHOT SIZE*/}
-          <MenuItem value="w/h">
             <Flex gap={"10px"}>
-              <Text>Width</Text>
-              <Input size={"sm"} placeholder="width" />
-              <Text>Height</Text>
-              <Input size={"sm"} placeholder="heigth" />
+              <Box>
+                <FormLabel>Width</FormLabel>
+                <Input size={"sm"} placeholder="width" />
+              </Box>
+              <Box>
+                <FormLabel>Height</FormLabel>
+                <Input size={"sm"} placeholder="heigth" />
+              </Box>
             </Flex>
-          </MenuItem>
         </MenuGroup>
         <MenuDivider />
-          {/* WAIT UNTIL*/}
+        {/* WAIT UNTIL*/}
         <MenuGroup defaultValue="load" title="Wait Until" type="radio">
           <RadioGroup defaultValue="load" colorScheme="green" p="10px">
             <Stack spacing={5} direction="row">
@@ -59,21 +62,109 @@ const UrlFilter = () => {
           </RadioGroup>
         </MenuGroup>
         <MenuDivider />
-          {/* DELAY*/}
-          {/* CSS INJUCTION*/}
-          {/* USER AGENT*/}
-          {/* HIDE POPUP*/}
-          {/* ACCEPT GDPR BANNER*/}
-          {/* EXPORT FORMAT*/}
-          {/* SELECTOR*/}
-          {/* ACCEPT LANG*/}
-          {/* DEVICE*/}
-          {/* EMULATE*/}
-          {/* DEVICE SCALE FACTOR*/}
-          {/* PREFERS COLOR SCHEME*/}
-          {/* QUALITY*/}
-          {/* CROP*/}
+        {/* EXPORT FORMAT*/}
+        <MenuGroup title="Export Format" type="radio">
+          <RadioGroup defaultValue="png" colorScheme="green" p="10px">
+            <Stack spacing={5} direction="row">
+              <Radio value="png">PNG</Radio>
+              <Radio value="jpeg">JPEG</Radio>
+              <Radio value="webp">WEBP</Radio>
+            </Stack>
+          </RadioGroup>
+        </MenuGroup>
+        <MenuDivider />
+        {/* DEVICE*/}
+        <MenuGroup title="Device" type="radio">
+          <RadioGroup defaultValue="laptop" colorScheme="green" p="10px">
+            <Stack spacing={5} direction="row">
+              <Radio value="laptop">Laptop</Radio>
+              <Radio value="mobile">Mobile</Radio>
+              <Radio value="tablet">Tablet</Radio>
+              <Radio value="desktop">Desktop</Radio>
+            </Stack>
+          </RadioGroup>
+        </MenuGroup>
+        <MenuDivider />
+        {/* PREFERS COLOR SCHEME*/}
+        <MenuGroup title="Prefers color scheme" type="radio">
+          <RadioGroup defaultValue="light" colorScheme="green" p="10px">
+            <Stack spacing={5} direction="row">
+              <Radio value="light">Light</Radio>
+              <Radio value="dark">Dark</Radio>
+            </Stack>
+          </RadioGroup>
+        </MenuGroup>
+        <MenuDivider />
 
+        <MenuGroup>
+            <Flex gap={"10px"}>
+              {/* DELAY*/}
+              <Box>
+                <FormLabel>Delay</FormLabel>
+                <Input size={"sm"} type="number" placeholder="delay" />
+              </Box>
+              {/* DEVICE SCALE FACTOR*/}
+              <Box>
+                <FormLabel>Device Scale Factor</FormLabel>
+                <Input
+                  size={"sm"}
+                  type="number"
+                  placeholder="Device scale factor"
+                />
+              </Box>
+            </Flex>
+            <Flex gap={"10px"}>
+              {/* CROP*/}
+              <Box>
+                <FormLabel>Crop</FormLabel>
+                <Input size={"sm"} placeholder="Crop" />
+              </Box>
+              {/* USER AGENT*/}
+              <Box>
+                <FormLabel>User agent</FormLabel>
+                <Input size={"sm"} placeholder="User agent" />
+              </Box>
+            </Flex>
+          
+            <Flex gap={"10px"}>
+              {/* CSS INJUCTION*/}
+              <Box>
+                <FormLabel>Css injuction</FormLabel>
+                <Input size={"sm"} placeholder="Css injuction" />
+              </Box>
+              {/* QUALITY*/}
+              <Box>
+                <FormLabel>Quality</FormLabel>
+                <Input
+                  type='number'
+                  size="sm"
+                  placeholder="0 - 100 (multiple of 10)"
+                />
+              </Box>
+            </Flex>
+            <Flex gap={"10px"}>
+              {/* SELECTOR*/}
+              <Box>
+                <FormLabel>Selector</FormLabel>
+                <Input size={"sm"} placeholder="Selector" />
+              </Box>
+              {/* ACCEPT LANG*/}
+              <Box>
+                <FormLabel>Accept lang</FormLabel>
+                <Input size={"sm"} placeholder="Accept lang" />
+              </Box>
+            </Flex>
+          <CheckboxGroup colorScheme="green">
+            <Stack spacing={"5"} p="10px">
+              {/* HIDE POPUP*/}
+              <Checkbox>Hide Popup</Checkbox>
+              {/* ACCEPT GDPR BANNER*/}
+              <Checkbox>Accept gdpr banner</Checkbox>
+            </Stack>
+          </CheckboxGroup>
+        </MenuGroup>
+        <MenuDivider />
+        {/* EMULATE*/}
       </MenuList>
     </Menu>
   );
